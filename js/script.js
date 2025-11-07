7864c81e1d71f1a525df2c4b351778269e11641a
Jomar Arais <araisjomar23-cmd@gmail.com>
2025-11-07 22:33:19 +0800

    Updated website content and design

--- js/script.js
+++ js/script.js
@@ -243,3 +243,10 @@ const openButton = document.getElementById('openProModal');
       }
     });
   }
+
+const menuToggle = document.querySelector('.menu-toggle');
+const navLinks = document.querySelector('.nav-links');
+
+menuToggle.addEventListener('click', () => {
+  navLinks.classList.toggle('active');
+});
